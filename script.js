4a1a23aff78fdfcf8471d67999c47e9e0e0dc46c
Strela196 <119959434+Strela196@users.noreply.github.com>
2024-06-28 18:44:30 +0200

    added a go to next page button

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+let pageNum = 1;
+
 const gridContainer = document.getElementById("gridcont");
-const APILINK =
-  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+let APILINK = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pageNum}&sort_by=popularity.desc`;
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
   "https://api.themoviedb.org/3/search/movie?&api_key=5c92c118674bd96710d8d45d19f3b792&query=";
 const form = document.getElementById("form");
 const query = document.getElementById("query");
-
+const buttonPage = document.getElementById("gotonextpage");
 const options = {
   method: "GET",
   headers: {
@@ -16,16 +17,18 @@ const options = {
   },
 };
 
-function makeNewSameDiv(url = "") {
+buttonPage.onclick = newPage;
+
+function loadMovies(url = "") {
   fetch(url, options)
     .then((response) => response.json())
     .then((data) => {
       console.log(data.results);
       data.results.forEach((element) => {
-        const newDiv = document.createElement('div');
-        const newH3 = document.createElement('h3');
-        const newImg = document.createElement('img');
-        newDiv.className = 'moviestuff';
+        const newDiv = document.createElement("div");
+        const newH3 = document.createElement("h3");
+        const newImg = document.createElement("img");
+        newDiv.className = "moviestuff";
         newH3.textContent = element.title;
         newImg.src = IMG_PATH + element.poster_path;
         gridContainer.appendChild(newDiv);
@@ -35,4 +38,17 @@ function makeNewSameDiv(url = "") {
     });
 }
 
-makeNewSameDiv(APILINK);
+loadMovies(APILINK);
+
+function newPage() {
+  clearMovies();
+  pageNum++;
+  const newAPILINK = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pageNum}&sort_by=popularity.desc`;
+  loadMovies(newAPILINK);
+}
+
+function clearMovies() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+}
